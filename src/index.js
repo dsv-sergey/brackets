6f049974216cc046fbc932d9fc05dbc23c28b393
Siarhei Dzeraviannik <33173033+dsv-sergey@users.noreply.github.com>
2019-09-23 00:49:22 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let steck = [];
+    function hasBrackets(bracket) {
+        bracketsConfig.forEach(el => {
+            if (el[0] === bracket) {
+                steck.push(el[1]);
+            } else if (el[1] === bracket) {
+                steck.push("false");
+            }
+        });
+    }
+    str.split("").forEach(el => {
+        el === steck[steck.length - 1] ? steck.pop() : hasBrackets(el);
+    });
+    return steck.length !== 0 ? false : true;
+};
